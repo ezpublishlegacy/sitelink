6147a4b340ac52239ba22a32c22f8f475fb1781b
Michael <michael@thinkcreative.com>
2011-04-14 12:44:34 -0400

    fix bug in eZURLType for when hasContent() returns a false and content exists

--- classes/sitelink/sitelinkurl.class.php
+++ classes/sitelink/sitelinkurl.class.php
@@ -3,7 +3,7 @@
 class SiteLinkURL implements SiteLinkDataTypeInterface
 {
 	public function modify($Attribute,$LinkType,$SiteLink){
-		if($Attribute->hasContent()){
+		if($Attribute->hasContent() || $Attribute->content()){
 			return $Attribute->content();
 		}
 		return false;
