af19caf5996f5c351cde2d8efa8e40a7a087586c
root <root@ip-10-117-41-94.ec2.internal>
2011-10-13 20:00:42 -0400

    prevent spaces in dowload filenames

--- classes/sitelink/sitelinkbinaryfile.class.php
+++ classes/sitelink/sitelinkbinaryfile.class.php
@@ -5,7 +5,7 @@ class SiteLinkBinaryFile implements SiteLinkDataTypeInterface
 	public function modify($Attribute,$LinkType,$SiteLink){
 		$ContentObjectID = $Attribute->ContentObjectID;
 		$ContentObjectAttributeID = $Attribute->ID;
-		$FileName = rawurlencode($Attribute->content()->OriginalFilename);
+		$FileName = rawurlencode(str_replace(" ", "_", $Attribute->content()->OriginalFilename));
 		return "content/download/$ContentObjectID/$ContentObjectAttributeID/".$FileName;
 	}
 }
