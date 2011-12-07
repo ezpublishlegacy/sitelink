b741f43473eb21b994833c80bae244c0868ab2c5
root <root@domU-12-31-39-00-09-12.compute-1.internal>
2011-12-07 14:29:47 -0500

    remove debug

--- classes/sitelink.class.php
+++ classes/sitelink.class.php
@@ -77,8 +77,6 @@ class SiteLink
 			$assigned_nodes = $object->assignedNodes();
 			array_unshift($assigned_nodes, $object->mainNode());
 			foreach($assigned_nodes as $node){
-				eZDebug::writeDebug($this->rootNodeID);
-				eZDebug::writeDebug($node->pathArray());
 				if($node->hiddenStatusString() != "Hidden" && in_array($this->rootNodeID,$node->pathArray())){return $node;}
 			}
 			return $object->mainNode();
